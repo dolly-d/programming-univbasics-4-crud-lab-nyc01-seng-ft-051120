496f511a8f26daa795553f77971e8409473aa6f8
Dolly Desir <dolly.desir@gmail.com>
2020-04-29 21:09:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,9 +28,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  on_repeat.unshift
-end
-
+  array.pop
+  
 def retrieve_element_from_index(array, index_number)
   
 end
